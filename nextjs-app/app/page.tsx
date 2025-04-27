d12b1d9498fbb3e3bdb4fea731bee471ab68f0e9
Adam Gee <adam@adamatronix.com>
2025-04-27 16:50:37 +0800

    fix: clean up Hero wrapper

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -16,19 +16,17 @@ import DubaiImage from "@/app/assets/images/dubai.webp";
 export default async function Page() {
   return (
     <>
-      <div className="bg-gradient-to-r from-red-200 from-0% via-white via-40%  relative">
-        <div className="bg-gradient-to-b from-white w-full h-40 absolute top-0"></div>
-        <div className="bg-gradient-to-t from-white w-full h-40 absolute bottom-0"></div>
+      <div className="relative">
         <Hero />
       </div>
       <CollectiveVision />
-      <div className="border-t border-gray-10">
+      {/* <div className="border-t border-gray-10">
         <div className="container">
           <aside className="py-12 sm:py-20">
             <Suspense>{await AllPosts()}</Suspense>
           </aside>
         </div>
-      </div>
+      </div> */}
       <OurPhilosophy />
       <div className="w-full aspect-[1.84] relative">
         <Image
