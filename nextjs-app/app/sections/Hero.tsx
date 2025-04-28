95078014dfa73f09d88093a5c06dc05878b1ae8d
Adam Gee <adam@adamatronix.com>
2025-04-28 12:01:39 +0800

    feat: add slight parallax and opacity effects to Hero

--- nextjs-app/app/sections/Hero.tsx
+++ nextjs-app/app/sections/Hero.tsx
@@ -2,7 +2,8 @@
 
 import Image from "next/image";
 import HeroImage from "@/app/assets/images/hero_image.webp";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useLenis } from "lenis/react";
 
 interface HeroProps {}
@@ -18,23 +19,36 @@ export default function Hero(props: HeroProps) {
     }
   }, [lenis]);
 
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], [0, 300]);
+  const opacity = useTransform(scrollYProgress, [0, 0.8], [1, 0.4]);
+
+  const discover_opacity = useTransform(scrollYProgress, [0.5, 0.8], [1, 0]);
   return (
-    <div className="relative min-h-svh">
-      <Image
-        className="object-[10%_50%] lg:object-center"
-        sizes="100vw"
-        objectFit="cover"
-        fill
-        src={HeroImage}
-        alt="Hero Image"
-      />
-      <div
+    <div className="relative min-h-svh bg-primary-background" ref={ref}>
+      <motion.div className="absolute size-full" style={{ y, opacity }}>
+        <Image
+          className="object-[10%_50%] lg:object-center"
+          sizes="100vw"
+          objectFit="cover"
+          fill
+          src={HeroImage}
+          alt="Hero Image"
+        />
+      </motion.div>
+
+      <motion.div
+        style={{ opacity: discover_opacity }}
         onClick={discoverClick}
         className="absolute z-10 flex flex-col items-center justify-center bottom-0 left-0 w-full gap-5 cursor-pointer text-white hover:text-white/60 transition-all duration-200 ease-in-out"
       >
         <div className="text-2xl italic">Discover</div>
         <div className="relative h-[50px] w-[1px] bg-white"></div>
-      </div>
+      </motion.div>
     </div>
   );
 }
