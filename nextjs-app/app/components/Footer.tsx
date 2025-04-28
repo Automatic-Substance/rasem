a623dbc25ab8d3c43f4863bb41893206c1f37479
Adam Gee <adam@adamatronix.com>
2025-04-28 22:29:59 +0800

    fix: footer stuff

--- nextjs-app/app/components/Footer.tsx
+++ nextjs-app/app/components/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="relative text-white bg-primary-background overflow-hidden">
-      <StaggerIn className="grid grid-cols-7 lg:grid-cols-9 gap-x-5 gap-y-12 lg:gap-x-12 lg:gap-y-12 mx-10 lg:mx-12 mt-14 mb-8 lg:my-20">
+      <StaggerIn className="grid grid-cols-7 lg:grid-cols-9 gap-x-5 gap-y-12 lg:gap-x-12 lg:gap-y-12 mx-10 lg:mx-12 mt-14 mb-8 lg:mt-20 lg:mb-10">
         <div className="col-span-7 lg:col-span-4 flex flex-col items-center lg:items-start justify-between">
           <motion.div variants={animation}>
             <RasemLogo className="w-[140px] lg:w-[200px]" />
           </motion.div>
           <motion.div variants={animation} className="hidden lg:block text-lg">
-            <p>Copyright © {year} Lorem Group Ltd.</p>
+            <p>Copyright © {year} RASEM Investments LLC</p>
             <p>All rights reserved.</p>
           </motion.div>
         </div>
