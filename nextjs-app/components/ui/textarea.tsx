4ea9f16468b43056f8c58a748813a934a859bf02
Adam Gee <adam@adamatronix.com>
2025-04-30 13:33:21 +0800

    fix: update input styling and submit form to netlify

--- nextjs-app/components/ui/textarea.tsx
+++ nextjs-app/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        "flex min-h-[60px] w-full outline-none rounded-none border-b border-white bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex min-h-[60px] w-full outline-none rounded-none border-b border-t border-white bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
       ref={ref}
